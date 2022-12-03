1a93a9016c745303acacf5b8c133a0e085c07c04
Nick Trevino <github@nicktrevino.com>
2022-12-03 09:29:19 -0700

    Copy Day 2 Part 1 to Day 2 Part 2.

---
+++ solutions/2022/2/2.ts
@@ -0,0 +1,71 @@
+
+import { input, log } from "@Hyperlisk/aoc-lib";
+
+const inputData = await input.fetchProblemInput(2022, 2);
+const data = input.parse<Array<string>>(
+  inputData,
+  (dataLine: string) => {
+    return input.parse<string>(dataLine, String, input.parse.split.space);
+  },
+  input.parse.split.line,
+);
+
+const THROWS = {
+  ROCK: 'rock',
+  PAPER: 'paper',
+  SCISSORS: 'scissors',
+};
+const THROW_MAP = {
+  'A': THROWS.ROCK,
+  'B': THROWS.PAPER,
+  'C': THROWS.SCISSORS,
+  'X': THROWS.ROCK,
+  'Y': THROWS.PAPER,
+  'Z': THROWS.SCISSORS,
+};
+const THROW_VALUES = {
+  [THROWS.ROCK]: 1,
+  [THROWS.PAPER]: 2,
+  [THROWS.SCISSORS]: 3,
+};
+
+const RESULT_VALUES = {
+  loss: 0,
+  draw: 3,
+  win: 6,
+};
+
+const sum = (numbers: Array<number>) => numbers.reduce((total, n) => total + n, 0);
+
+const gameResult = (opponent: string, me: string) => {
+  const loss = (THROW_MAP[opponent] === THROWS.ROCK && THROW_MAP[me] === THROWS.SCISSORS) ||
+               (THROW_MAP[opponent] === THROWS.PAPER && THROW_MAP[me] === THROWS.ROCK) ||
+               (THROW_MAP[opponent] === THROWS.SCISSORS && THROW_MAP[me] === THROWS.PAPER);
+  if (loss) {
+    return THROW_VALUES[THROW_MAP[me]] + RESULT_VALUES.loss;
+  }
+  const draw = (THROW_MAP[opponent] === THROWS.ROCK && THROW_MAP[me] === THROWS.ROCK) ||
+               (THROW_MAP[opponent] === THROWS.PAPER && THROW_MAP[me] === THROWS.PAPER) ||
+               (THROW_MAP[opponent] === THROWS.SCISSORS && THROW_MAP[me] === THROWS.SCISSORS);
+  if (draw) {
+    return THROW_VALUES[THROW_MAP[me]] + RESULT_VALUES.draw;
+  }
+  const win = (THROW_MAP[opponent] === THROWS.ROCK && THROW_MAP[me] === THROWS.PAPER) ||
+              (THROW_MAP[opponent] === THROWS.PAPER && THROW_MAP[me] === THROWS.SCISSORS) ||
+              (THROW_MAP[opponent] === THROWS.SCISSORS && THROW_MAP[me] === THROWS.ROCK);
+  if (win) {
+    return THROW_VALUES[THROW_MAP[me]] + RESULT_VALUES.win;
+  }
+  throw new Error(`Unkown values: [opponent:${opponent}] [me:${me}]`);
+}
+
+function solve(fixedGames: Array<Array<string>>) {
+  const gameResults = fixedGames.map((fixedGame, i) => {
+    const [opponentThrow, myThrow] = fixedGame;
+    return gameResult(opponentThrow, myThrow);
+  });
+  return sum(gameResults);
+}
+
+const solution = solve(data);
+log.write(solution);
