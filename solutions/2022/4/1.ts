5365397265d9c1b501ddead5e80014014c5d6cb9
Nick Trevino <github@nicktrevino.com>
2022-12-05 22:30:47 -0700

    Update solution for Day 4 to just use range ends.
    I mentioned in the initial commit for this solution that we should just
    check the ends. We are given the ends as our input. So this removes the
    virtual array usage since we did not actually need it.

--- solutions/2022/4/1.ts
+++ solutions/2022/4/1.ts
@@ -12,11 +12,7 @@ const data = input.parse(
 function solve(rangesList: Array<Array<Array<number>>>) {
   let result = 0;
   rangesList.forEach(ranges => {
-    const [rangeOnePoints, rangeTwoPoints] = ranges;
-    const rangeOne = array.virtualRange.inclusive(rangeOnePoints[0], rangeOnePoints[1]);
-    const [rangeOneStart, rangeOneEnd] = [rangeOne[0], rangeOne[rangeOne.length - 1]];
-    const rangeTwo = array.virtualRange.inclusive(rangeTwoPoints[0], rangeTwoPoints[1]);
-    const [rangeTwoStart, rangeTwoEnd] = [rangeTwo[0], rangeTwo[rangeTwo.length - 1]];
+    const [[rangeOneStart, rangeOneEnd], [rangeTwoStart, rangeTwoEnd]] = ranges;
     const hasCompleteOverlap =
       (rangeOneStart >= rangeTwoStart && rangeOneStart <= rangeTwoEnd && rangeOneEnd >= rangeTwoStart && rangeOneEnd <= rangeTwoEnd) ||
       (rangeTwoStart >= rangeOneStart && rangeTwoStart <= rangeOneEnd && rangeTwoEnd >= rangeOneStart && rangeTwoEnd <= rangeOneEnd);
