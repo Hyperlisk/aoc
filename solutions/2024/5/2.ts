a1b1bc8d041cb6a95c93b46723efe0df65ece2b6
Nick Trevino <github@nicktrevino.com>
2024-12-11 21:15:39 -0700

    2024 Day 5

---
+++ solutions/2024/5/2.ts
@@ -0,0 +1,42 @@
+
+import { comparator, input, log } from "@Hyperlisk/aoc-lib";
+
+type InputType = [ORDER: [A: number, B: number][], UPDATES: number[][]];
+
+const inputData = await input.fetchProblemInput(2024, 5);
+const data: InputType = input.parse(
+  inputData,
+  input.parse.split.group,
+  (group: string) => input.parse(group, input.parse.split.line, (line) => line.split(/[,|]/).map((n) => parseInt(n))),
+) as InputType;
+
+function solve() {
+  let result = 0;
+
+  let cmp: typeof comparator.generic<number> = (a, b) => {
+    return comparator.numbers(a, b);
+  };
+  const [ORDER, UPDATES] = data;
+  ORDER.forEach(([A, B]) => {
+    const oldCmp = cmp;
+    cmp = (a, b) => {
+      if (a === A && b === B) {
+        return -1;
+      }
+      if (a === B && b === A) {
+        return 1;
+      }
+      return oldCmp(a, b);
+    };
+  });
+  UPDATES.forEach((update) => {
+    const clone = update.slice(0);
+    clone.sort(cmp);
+    if (comparator.generic(update, clone) !== 0) {
+      result += clone[(clone.length - 1) >> 1];
+    }
+  });
+  return result;
+}
+
+log.write(solve());
